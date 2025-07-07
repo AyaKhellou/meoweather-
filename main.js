89facb946016f402d935d94cce459bceec038706
Aya <aya.khellou@gmail.com>
2025-07-07 08:43:51 +0100

    final edits

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-console.log("KEY:", import.meta.env.VITE_WEATHER_API_KEY);
-
 let searchInput = document.querySelector('.searchBar input');
 
 let searchIcon = document.querySelector('.searchBar i');
@@ -22,25 +20,40 @@ let details = [temperature ,windSpeed ,humidity ,sunriseHolder ,sunsetHolder];
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 
+
+import icon01d from './images/sun.png';
+import icon01n from './images/moon.png';
+import icon02d from './images/sunny.png';
+import icon02n from './images/night.png';
+import icon03 from './images/cloud(1).png';
+import icon04 from './images/clouds.png';
+import icon09 from './images/heavy-rain.png';
+import icon10d from './images/cloudy.png';
+import icon10n from './images/rainy-night.png';
+import icon11 from './images/storm.png';
+import icon13 from './images/snow.png';
+import icon50 from './images/mist.png';
+import noResultIcon from './images/no-results.png'; 
+
 const customIcons = {
-    "01d": "sun.png",
-    "01n": "moon.png",
-    "02d": "sunny.png",
-    "02n": "night.png",
-    "03d": "cloud(1).png",
-    "03n": "cloud(1).png",
-    "04d": "clouds.png",
-    "04n": "clouds.png",
-    "09d": "heavy-rain.png",
-    "09n": "heavy-rain.png",
-    "10d": "cloudy.png",
-    "10n": "rainy-night.png",
-    "11d": "storm.png",
-    "11n": "storm.png",
-    "13d": "snow.png",
-    "13n": "snow.png",
-    "50d": "mist.png",
-    "50n": "mist.png",
+  "01d": icon01d,
+  "01n": icon01n,
+  "02d": icon02d,
+  "02n": icon02n,
+  "03d": icon03,
+  "03n": icon03,
+  "04d": icon04,
+  "04n": icon04,
+  "09d": icon09,
+  "09n": icon09,
+  "10d": icon10d,
+  "10n": icon10n,
+  "11d": icon11,
+  "11n": icon11,
+  "13d": icon13,
+  "13n": icon13,
+  "50d": icon50,
+  "50n": icon50
 };
 
 searchIcon.addEventListener("click", handleSearch);
@@ -68,7 +81,8 @@ function getData(city) {
 
         weatherDescription.textContent = `hi, ${myData.weather[0].description} today 😊`;
 
-        weatherIcon.src = "images/" + (customIcons[myData.weather[0].icon] || "sun.png");
+        weatherIcon.src = customIcons[myData.weather[0].icon] || icon01d;
+
 
         weatherIcon.alt = myData.weather[0].description;
 
@@ -103,7 +117,8 @@ function getData(city) {
     .catch((err)=>{
 
         weatherDescription.textContent = "we're unable to find the city you entred";
-        weatherIcon.src = "images/no-results.png";
+        weatherIcon.src = noResultIcon;
+
         details.forEach(div => {
             div.textContent = "--";
         });
